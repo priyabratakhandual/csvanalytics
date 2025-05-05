754877822a7392d952deda293aab4e16e4154396
priyabratakhandual <priyabratakhandual7@gmail.com>
2025-05-05 16:47:11 +0530

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
 from app import app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from flask import Flask
+
+# Mount your app under /csv-analytics
+application = DispatcherMiddleware(Flask('dummy'), {
+    '/csv-analytics': app
+})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    run_simple('0.0.0.0', 5001, application)
